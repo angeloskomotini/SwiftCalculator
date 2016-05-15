576c4a8d31892d8d0929f5221fa93515d4072384
angeloskomotini <angeloskomotini@gmail.com>
2016-05-16 00:30:49 +0300

    Initial Commit

---
+++ SwiftCalculator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftCalculator
+//
+//  Created by Angelos Staboulis on 16/05/16.
+//  Copyright © 2016 Angelos Staboulis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftCalculator
+//
+//  Created by Angelos Staboulis on 16/05/16.
+//  Copyright © 2016 Angelos Staboulis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
